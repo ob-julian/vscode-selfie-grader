a97c2114220fec5671b2411729057c3510140eee
Julian <julianoberhofer2@gmail.com>
2023-04-07 14:03:31 +0200

    refined interface

--- README.md
+++ README.md
@@ -6,12 +6,15 @@ This is a VSCode extension that allows you to run the grader for the [selfie sys
 
 ### Buttons
 
-If in a Workspace with /selfie/ as the current working directory, the extension adds two buttons at the upper right corner of the editor. \
+If in a Workspace with /selfie/ in the name of the current working directory, the extension adds two buttons at the upper right corner of the editor. \
 >Hint: The two buttons are compressed into one button and a dropdown/select menu by VSCode. 
 
 The Labels of the buttons are:
 * `▷ Run Grader`: Runs the grader 
 * `⚙ Grader Options`: Lets you select the assignment you want to run the grader for.
+
+The `▷ Run Grader` button has the option, if you hover over it, to press `Shift/Alt` to activate the `⚙ Grader Options` button instead. \
+This should make it more convenient to select a different assignment to run the grader for.
 
 ### Commands
 
@@ -29,7 +32,7 @@ Furthermore, if you run `Grader Options` and select an assignment, the extension
 
 ## Requirements
 
-This extension only works if /selfie/ is the current working directory. Otherwise, all functionality is disabled, to make sure that the extension does not interfere with other projects. \
+This extension only works if the current working directory contains /selfie/ somewhere in the name. Otherwise, all functionality is disabled, to make sure that the extension does not interfere with other projects. \
 Furthermore, the requirements for the selfie system apply.\
  E.g. Python3, C Compiler, ... must be installed and working.
 
@@ -37,17 +40,18 @@ Furthermore, the requirements for the selfie system apply.\
 
 This extension contributes the following settings:
 
-* `selfie-grader.clearPreviousOutput:`: Whether to clear previous output before each run.  (Default is __true__)
+* `selfie-grader.clearPreviousOutput`: Whether to clear previous output before each run.  (Default is __true__)
 * `selfie-grader.runInTerminal`: Whether to run the grader in Integrated Terminal. (Default is __false__)
 * `selfie-grader.runInTerminal`: Whether to preserve focus on the code editor after the grader is triggered. (Default is __true__)
+* `selfie-grader.option`: Options to pass to the grader. (Default is __empty__) \
+Possibilities (that are interesting for students) are `-q` for print grade only and `-s` for do not show the processing spinner.
 
 ## Known Issues
 
 * If selecting the `Grader Options` button from the Dropdown Menu, the `Grader Options` is now the active Button that is shown, even though the `Run Grader` button would be the more convenient one to be active. \
 This is the default behavior of the VSCode API and I have not found a way to change it.
 * If running the extension with other extensions like `Code Runner` and/or `C/C++` installed, the Dropdown Menu can get cramped with the functionality/buttons of the other extensions. \
-My suggestion is to disable the other extensions in this workspace, as they are not needed for the selfie system anyways (Correct me if I am wrong). \
-* If running `Run Grader` in the Output Channel on Windows, the Output will be a unreadable mess. This is a issue with the grader itself and not the extension. \
+My suggestion is to disable the other extensions in this workspace, as they are not needed for the selfie system anyways (Execpt IntelliSense for C) (Correct me if I am wrong). \
 
 ## Why
 
--- extension.js
+++ extension.js
@@ -1,118 +1,152 @@
 const vscode = require('vscode');
 
+let outputChannel;
+let errorChannel;
+let terminal;
+
 /**
  * @param {vscode.ExtensionContext} context
  */
 function activate(context) {
-	console.log('selfie-grader is now active!');
-
-	let outputChannel;
-	let errorChannel;
-	let terminal;
+	const disclaimer1 = 'VSCodes output channel does not support ANSI escape codes, so the output is not colored.';
+	const disclaimer2 = 'Change the setting "selfie-grader.runInTerminal" to true to switch to the terminal, which supports ANSI escape codes.';
+	let out = disclaimer1 + '\n' + disclaimer2 + '\n\n';
+	let grading = false;
 
 	const path = require('path');
 	const workspaceRoot = vscode.workspace.workspaceFolders[0].uri.fsPath;
-	const pathToPy = path.join(workspaceRoot, 'grader', 'self.py');
+	const absolutePathToPyProgramm = path.join(workspaceRoot, 'grader', 'self.py');
+	const relativePathToPyProgramm = path.join('grader','self.py')
 
 	const assignments = [];
 	let selected = '';
 
-	const config = vscode.workspace.getConfiguration('selfie-grader');
-
-
 	let runGraderCommand = vscode.commands.registerCommand('selfie-grader.runGrader', function () {
-		if(vscode.workspace.name !== 'selfie'){
+		if (grading) {
+			vscode.window.showErrorMessage('Grader is already running');
+			return;
+		}
+		if(!isInSelfie()){
 			vscode.window.showErrorMessage('Please open the selfie folder as workspace');
 			return;
 		}
 		if(vscode.workspace.isTrusted === false){
-			vscode.window.showErrorMessage('Executing the Selfie Grader disabled in a untrusted workspace');
+			vscode.window.showErrorMessage('Executing the Selfie Grader is disabled in a untrusted workspace');
 			return;
 		}
+		grading = true;
 
 
-		const clearPreviousOutput = config.get('clearPreviousOutput') === true;
-		const preserveFocus = config.get('preserveFocus') === true;
+		const clearPreviousOutput = vscode.workspace.getConfiguration('selfie-grader').get('clearPreviousOutput') === true;
+		const preserveFocus = vscode.workspace.getConfiguration('selfie-grader').get('preserveFocus') === true;
+		const options = vscode.workspace.getConfiguration('selfie-grader').get('options');
 
 		//const exec = require('child_process').exec;
 		//const pythonFileRelativePath = path.relative(workspaceRoot, path.join(currentDir, 'grader', 'self.py'));
 		if(selected === ''){
 			vscode.commands.executeCommand('selfie-grader.graderOptions',);
+			grading = false;
 		}
-		else if(config.get('runInTerminal') === true){
+		else if(vscode.workspace.getConfiguration('selfie-grader').get('runInTerminal') === true){
 			//run the command in a terminal
 
 			if(terminal === undefined){
 				terminal = vscode.window.createTerminal('Grader');
 			}
+			//setup
+			terminal.sendText(''); //to dispose of the previous text in the terminal
+			terminal.sendText('cd \'' + workspaceRoot + '\'');
 
+			//clean up
 			if(clearPreviousOutput){
 				terminal.sendText('clear');
 			}
-			terminal.sendText('python3 \'' + pathToPy + '\' ' + selected);
-
-			/*vscode.commands.executeCommand('workbench.action.terminal.sendSequence', {
-			text: `${'python3 \'' + pathToPy + '\' ' + selected}`
-			});*/
+			if(outputChannel !== undefined || errorChannel !== undefined){
+				outputChannel.hide();
+				errorChannel.hide();
+			}
+			terminal.show(preserveFocus);
+			terminal.sendText('python3 \'' + relativePathToPyProgramm + '\' ' + selected + ' ' + options);
+			//5 sec timeout to prevent spamming the grader
+			setTimeout(function(){
+				grading = false;
+			}, 5000);
 			terminal.show(preserveFocus);
 		}
 		else {
 			//run the command in the output channel
-			const clearPreviousOutput = config.get('clearPreviousOutput') === true;
-			const preserveFocus = config.get('preserveFocus') === true;
 
-			if(outputChannel === undefined){
-				outputChannel = vscode.window.createOutputChannel('Grader Output');
-			}
 			if(errorChannel === undefined){
 				errorChannel = vscode.window.createOutputChannel('Grader Error');
 			}
+			if(outputChannel === undefined){
+				outputChannel = vscode.window.createOutputChannel('Grader Output');
+			}
+			//clean up
 			if(clearPreviousOutput){
 				outputChannel.clear();
 				errorChannel.clear();
+				out = disclaimer1 + '\n' + disclaimer2 + '\n\n';
 			}
+			if(terminal !== undefined){
+				terminal.hide();
+			}
+			outputChannel.show(preserveFocus);
+
 			const { spawn } = require('child_process');
 
-			const pythonProcess = spawn('python3', [pathToPy, selected]);
+			//disclaimer: VSCodes output channel does not support ANSI escape codes, so the output is not colored
+			
+			const info = 'Running grader for assignment ' + selected + '...';
+			outputChannel.appendLine(disclaimer1);
+			outputChannel.appendLine(disclaimer2);
+			outputChannel.appendLine('');
+			outputChannel.appendLine(info);
+
+			const pythonProcess = spawn('python3', [relativePathToPyProgramm, selected, options], { cwd: workspaceRoot });
 
 			let stdoutUsed = false;
 			let stderrUsed = false;
+			if(out != disclaimer1 + '\n' + disclaimer2 + '\n\n'){
+				out = out + '\n' + '-'.repeat(50) + '\n\n';
+			}
+			out = out + info + '\n';
 
 			pythonProcess.stdout.on('data', (data) => {
-				const dataString  = data.toString('utf-8');
-				outputChannel.append(dataString);
-				stdoutUsed = true;
+				if(data.includes('\b')) {
+					out = out.split('\n[  |   \]')[0];;
+					out = out.replace(/\r/g, '');
+					outputChannel.replace(out);
+					return;
+				}
+				const dataString  = cleanData(data);
+				if(dataString !== ''){
+					out = out + dataString + "\n";
+					outputChannel.append(dataString);
+					stdoutUsed = true;
+				}
 			});
 
 			pythonProcess.stderr.on('data', (data) => {
-				const dataString  = data.toString('utf-8');
+				const dataString  = cleanData(data);
 				errorChannel.append(dataString);
 				stderrUsed = true;
 			});
 
 			pythonProcess.on('close', (code) => {
-				//console.log(`child process exited with code ${code}`);
+				outputChannel.replace(out);
 				if(stderrUsed){
+					if(code !== 0)
+						errorChannel.appendLine('Exited with code ' + code);
 					errorChannel.show(preserveFocus);
 				}
 				else if(stdoutUsed){
 					outputChannel.show(preserveFocus);
 				}
+				grading = false;
 			});
 
-			/*exec('python3 "' + pathToPy + '" ' + selected, (error, stdout, stderr) => {
-				if(stdout !== ''){
-					outputChannel.append(stdout);
-					outputChannel.show(preserveFocus);
-				}
-				else{
-					errorChannel.append(stderr);
-					errorChannel.show(preserveFocus);
-				}
-			});*/
-
 		}
-
 	});
 
 	context.subscriptions.push(runGraderCommand);
@@ -121,7 +155,7 @@ function activate(context) {
 	const fs = require('fs');
 
 
-	const readStream = fs.createReadStream(pathToPy, { encoding: 'utf-8' });
+	const readStream = fs.createReadStream(absolutePathToPyProgramm, { encoding: 'utf-8' });
 
 	readStream.on('data', function(chunk) {
 		const lines = chunk.toString().split('\n'); //(chunk can be Buffer or String) so toString() is to avoid that a Buffer split (which does not exist) is called
@@ -149,17 +183,37 @@ function activate(context) {
 
 
 	const showOptionsCommand = vscode.commands.registerTextEditorCommand('selfie-grader.graderOptions', () => {
-		if(vscode.workspace.name !== 'selfie'){
+		if(!isInSelfie()){
 			vscode.window.showErrorMessage('Please open the selfie folder as workspace');
 			return;
 		}
 		if(vscode.workspace.isTrusted === false){
-			vscode.window.showErrorMessage('Executing the Selfie Grader disabled in a untrusted workspace');
+			vscode.window.showErrorMessage('Executing the Selfie Grader is disabled in a untrusted workspace');
+			return;
+		}
+
+		if(assignments.length === 0){
+			vscode.window.showErrorMessage('No assignments yet available');
 			return;
 		}
 		
 		const quickPick = vscode.window.createQuickPick();
 		quickPick.items = assignments.map(label => ({ label }));
+		quickPick.placeholder = 'Select an assignment';
+		quickPick.canSelectMany = false;
+		//quickPick.selectedItems = assignments.map(label => ({ label })); TODO: select the last selected item
+		/*if (selected) {
+			const selectedItem = quickPick.items.find(item => item.label === selected);
+			if (selectedItem) {
+				quickPick.selectedItems = [selectedItem];
+				console.log(selectedItem);
+			}
+		}*/
+		quickPick.onDidHide(() => quickPick.dispose());
+		quickPick.onDidAccept(() => {
+			quickPick.dispose();
+			vscode.commands.executeCommand('selfie-grader.runGrader',);
+		});
 		quickPick.onDidChangeSelection(selection => {
 			if (selection[0]) {
 				//vscode.window.showInformationMessage(`You selected ${selection[0].label}`);
@@ -181,4 +235,25 @@ module.exports = {
 	deactivate
 }
 
-//
+function isInSelfie(){
+	return vscode.workspace.name.includes('selfie');
+}
+
+function cleanData(data){
+	//removes all ANSI escape codes from the data
+	let dataString  = data.toString('utf-8');
+	const regex = /(\x9B|\x1B\[)[0-?]*[ -\/]*[@-~]/g;
+	dataString = dataString.replace(regex, '');
+	//removing other control characters
+	dataString = dataString.replace(/\n$/g, '');
+	dataString = dataString.replace(/\r/g, '');
+	//spacing Tests:
+	dataString = dataString.replace(/\[FAILED\]/g, '\n[FAILED]');
+	dataString = dataString.replace(/\[PASSED\]/g, '\n[PASSED]');
+	dataString = dataString.replace(/\[  |   \]/g, '\n[  |   \]');
+	dataString = dataString.replace(/executing test/g, '\nexecuting test');
+	return dataString.replace(/\b/g, '');
+
+
+	//return dataString.replace(/\n$/g, '');
+}
